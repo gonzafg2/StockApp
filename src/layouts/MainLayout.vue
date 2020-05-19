145b1a27ca690073f437e74d6b46ce1fa83f8897
Gonzalo Fleming <gonzafg2@gmail.com>
2020-05-19 03:07:37 -0400

    Cambios menores de traducción

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -15,7 +15,7 @@
           Control de Existencias
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div>Kipreos Ingenieros</div>
       </q-toolbar>
     </q-header>
 
@@ -30,7 +30,7 @@
           header
           class="text-grey-8"
         >
-          Essential Links
+          Enlaces de Interés
         </q-item-label>
         <EssentialLink
           v-for="link in essentialLinks"
@@ -61,47 +61,47 @@ export default {
       leftDrawerOpen: false,
       essentialLinks: [
         {
-          title: 'Docs',
+          title: 'ENTRADAS',
           caption: 'quasar.dev',
           icon: 'school',
           link: 'https://quasar.dev'
         },
         {
-          title: 'Github',
+          title: 'SALIDAS',
           caption: 'github.com/quasarframework',
           icon: 'code',
           link: 'https://github.com/quasarframework'
         },
         {
-          title: 'Discord Chat Channel',
+          title: 'STOCK',
           caption: 'chat.quasar.dev',
           icon: 'chat',
           link: 'https://chat.quasar.dev'
         },
-        {
-          title: 'Forum',
-          caption: 'forum.quasar.dev',
-          icon: 'record_voice_over',
-          link: 'https://forum.quasar.dev'
-        },
-        {
-          title: 'Twitter',
-          caption: '@quasarframework',
-          icon: 'rss_feed',
-          link: 'https://twitter.quasar.dev'
-        },
-        {
-          title: 'Facebook',
-          caption: '@QuasarFramework',
-          icon: 'public',
-          link: 'https://facebook.quasar.dev'
-        },
-        {
-          title: 'Quasar Awesome',
-          caption: 'Community Quasar projects',
-          icon: 'favorite',
-          link: 'https://awesome.quasar.dev'
-        }
+        // {
+        //   title: 'Forum',
+        //   caption: 'forum.quasar.dev',
+        //   icon: 'record_voice_over',
+        //   link: 'https://forum.quasar.dev'
+        // },
+        // {
+        //   title: 'Twitter',
+        //   caption: '@quasarframework',
+        //   icon: 'rss_feed',
+        //   link: 'https://twitter.quasar.dev'
+        // },
+        // {
+        //   title: 'Facebook',
+        //   caption: '@QuasarFramework',
+        //   icon: 'public',
+        //   link: 'https://facebook.quasar.dev'
+        // },
+        // {
+        //   title: 'Quasar Awesome',
+        //   caption: 'Community Quasar projects',
+        //   icon: 'favorite',
+        //   link: 'https://awesome.quasar.dev'
+        // }
       ]
     }
   }
