12cdbc11944a8b30e026037d7628e91f618470d1
Gonzalo Fleming <gonzafg2@gmail.com>
2020-05-19 02:53:41 -0400

    Carga Inicial

---
+++ src/boot/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+
+// Agregar configuración firebase:
+var firebaseConfig = {
+  apiKey: "AIzaSyBZsafvMdCvFeif3UW0Ya4CoJD8temnyoQ",
+  authDomain: "stock-kipreos.firebaseapp.com",
+  databaseURL: "https://stock-kipreos.firebaseio.com",
+  projectId: "stock-kipreos",
+  storageBucket: "stock-kipreos.appspot.com",
+  messagingSenderId: "1060322624632",
+  appId: "1:1060322624632:web:90665d122aff98fe91d98d",
+  measurementId: "G-B9B8XZYN6H"
+};
+
+let firebaseApp = firebase.initializeApp(firebaseConfig);
+let db = firebase.firestore();
+
+export { db, firebase };
