730dd0e2769891b1cdc67e9a0096a31d482a23cb
Gonzalo Fleming <gonzafg2@gmail.com>
2020-05-20 17:08:53 -0400

    Modifies in manifiest app

--- quasar.conf.js
+++ quasar.conf.js
@@ -109,9 +109,9 @@ module.exports = function (/* ctx */) {
         workboxPluginMode: "GenerateSW", // 'GenerateSW' or 'InjectManifest'
         workboxOptions: {}, // only for GenerateSW
         manifest: {
-          name: "Quasar App",
-          short_name: "Quasar App",
-          description: "A Quasar Framework app",
+          name: "Stock App",
+          short_name: "Stock App",
+          description: "An app for management of stock of products.",
           display: "standalone",
           orientation: "portrait",
           background_color: "#ffffff",
