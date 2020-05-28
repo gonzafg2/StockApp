dfea34c34251bd5e3df907f8261b15d4e31682f1
Gonzalo Fleming <gonzafg2@gmail.com>
2020-05-28 00:16:00 -0400

    Change parameters of firebase

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -5,27 +5,29 @@ import "firebase/performance";
 
 // Agregar configuración firebase:
 var firebaseConfig = {
-  apiKey: "AIzaSyBZsafvMdCvFeif3UW0Ya4CoJD8temnyoQ",
-  authDomain: "stock-kipreos.firebaseapp.com",
-  databaseURL: "https://stock-kipreos.firebaseio.com",
-  projectId: "stock-kipreos",
-  storageBucket: "stock-kipreos.appspot.com",
-  messagingSenderId: "1060322624632",
-  appId: "1:1060322624632:web:90665d122aff98fe91d98d",
-  measurementId: "G-B9B8XZYN6H"
+  apiKey: "AIzaSyA1k-FqCuiKFkcJwaWDChnZ7GY1NZv6DK0",
+  authDomain: "stockapp-4f3c2.firebaseapp.com",
+  databaseURL: "https://stockapp-4f3c2.firebaseio.com",
+  projectId: "stockapp-4f3c2",
+  storageBucket: "stockapp-4f3c2.appspot.com",
+  messagingSenderId: "189169559056",
+  appId: "1:189169559056:web:2f1677bd79b4faee588c95",
+  measurementId: "G-4YH5FK7TGC"
 };
 
 let firebaseApp = firebase.initializeApp(firebaseConfig);
 // let db = firebase.firestore();
 const perf = firebase.performance();
 
+// const analytics = firebase.analytics();
+
 var db = firebaseApp.firestore();
 
-if (location.hostname === "localhost") {
-  db.settings({
-    host: "localhost:8080",
-    ssl: false
-  });
-}
+// if (location.hostname === "localhost") {
+//   db.settings({
+//     host: "localhost:8080",
+//     ssl: false
+//   });
+// }
 
 export { db, firebase };
